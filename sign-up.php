94e124995e2099ef1805b9e5fcdb87d18b5d7a83
AyKrimino <ayoub.krimi14@gmail.com>
2025-04-07 23:24:42 +0100

    feat: add sign-up design and refactor code in sign-in page

--- sign-up.php
+++ sign-up.php
@@ -4,25 +4,26 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./assets/css/styles.css">
-        <link rel="stylesheet" href="./assets/css/sign-in-styles.css">
-        <title>Sign In</title>
+        <link rel="stylesheet" href="./assets/css/sign-up-styles.css">
+        <title>Sign Up</title>
     </head>
     <body>
         <?php include("./includes/header.php"); ?>
         <section class="auth-container">
             <div class="">
-                <form class="credentials-section" action="">
-                    <h1>Sign in</h1>
-                    <input type="email" name="email" id="email" placeholder="Email" />
-                    <input type="password" name="password" id="password" placeholder="Password" />
-                    <a href="#">Forgot you password?</a>
-                    <button type="submit">SIGN IN</button>
-                </form>
                 <div class="helper-section">
-                    <h1>Hello, Friend!</h1>
-                    <p>Enter your personal details and start journey with us.</p>
-                    <a class="redirect-btn" href="./sign-up.php">SIGN UP</a>
+                    <h1>Welcome, Back!</h1>
+                    <p>To keep connected with us please login with your personal info.</p>
+                    <a href="./sign-in.php" class="redirect-btn">SIGN IN</a>
                 </div>
+                <form class="credentials-section" action="">
+                    <h1>Create Account</h1>
+                    <input type="email" name="email" id="email" placeholder="Email" />
+                    <input type="text" name="name" id="name" placeholder="Name" />
+                    <input type="text" name="phone" id="phone" placeholder="Phone" />
+                    <input type="password" name="password" id="password" placeholder="Password" />
+                    <button type="submit">SIGN UP</button>
+                </form>
             </div>
         </section>
         <?php include("./includes/footer.php"); ?>
