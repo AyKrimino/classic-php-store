09b570c9c00af677f9c52dc40e060c5068c7db12
AyKrimino <ayoub.krimi14@gmail.com>
2025-04-19 10:31:41 +0100

    feat: add get customer functionality to admin customers edit page

--- admin-customers-edit.php
+++ admin-customers-edit.php
@@ -5,6 +5,20 @@ include_once($_SERVER["DOCUMENT_ROOT"] . "/classic-php-store/config/db_connectio
 if (!isset($_SESSION["user_role"]) && $_SESSION["user_role"] !== "admin") {
     header("location:admin-sign-in.php");
 }
+
+if (!isset($_GET["id"])) {
+    header("location:admin-customers.php");
+} else {
+    $userID = $_GET["id"];
+}
+
+function loadCustomer($connection, $userID) {
+    $query = "select * from User, Customer where User.user_id = $userID and Customer.user_id = $userID";
+    $res = mysqli_query($connection, $query);
+    return mysqli_fetch_assoc($res);
+}
+
+$customer = loadCustomer($connection, $userID);
 ?>
 
 <!DOCTYPE html>
@@ -21,16 +35,16 @@ if (!isset($_SESSION["user_role"]) && $_SESSION["user_role"] !== "admin") {
         <main class="main-section">
             <?php include_once("./includes/admin_sidebar.php"); ?>
             <section class="content-section">
-                <form method="POST" action="admin-customers-edit.php">
+                <form method="POST" action="admin-customers-edit.php?id=<?php echo $userID; ?>">
                     <h1>Customer Edit</h1>
-                    <input type="text" name="user-id" id="user-id" value="1" readonly />
-                    <input type="text" name="name" id="name" placeholder="Name" required />
-                    <input type="email" name="email" id="email" placeholder="Email" required />
-                    <input type="text" name="phone" id="phone" placeholder="Phone" />
-                    <input type="text" name="address" id="address" placeholder="Address" required />
+                    <input type="text" name="user-id" id="user-id" value="<?php echo $userID; ?>" readonly />
+                    <input value="<?php echo $customer["name"]; ?>" type="text" name="name" id="name" placeholder="Name" required />
+                    <input value="<?php echo $customer["email"]; ?>" type="email" name="email" id="email" placeholder="Email" required />
+                    <input value="<?php echo $customer["phone"]; ?>" type="text" name="phone" id="phone" placeholder="Phone" />
+                    <input value="<?php echo $customer["address"]; ?>" type="text" name="address" id="address" placeholder="Address" required />
                     <input type="text" name="role" id="role" value="Customer" readonly />
-                    <input type="date" name="created-at" id="created-at" value="2024-03-14" readonly />
-                    <input type="date" name="updated-at" id="updated-at" value="2024-03-14" readonly />
+                    <input type="date" name="created-at" id="created-at" value="<?php echo date("Y-m-d", strtotime($customer["created_at"])); ?>" readonly />
+                    <input type="date" name="updated-at" id="updated-at" value="<?php echo date("Y-m-d", strtotime($customer["updated_at"])); ?>" readonly />
                     <button type="submit" name="edit" id="edit">Edit</button>
                 </form>
             </section>
--- admin-customers.php
+++ admin-customers.php
@@ -96,21 +96,23 @@ $customers = loadCustomers($connection);
                             <td><?php echo htmlspecialchars($customer['address']); ?></td>
                             <td><?php echo date('Y‑m‑d', strtotime($customer['created_at'])); ?></td>
                             <td class="actions">
-                                <svg 
-                                    class="lucide lucide-pencil-line-icon lucide-pencil-line edit"
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    width="24" 
-                                    height="24" 
-                                    viewBox="0 0 24 24" 
-                                    fill="none" 
-                                    stroke="currentColor" 
-                                    stroke-width="2" 
-                                    stroke-linecap="round" 
-                                    stroke-linejoin="round">
-                                    <path d="M12 20h9"/>
-                                    <path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z"/>
-                                    <path d="m15 5 3 3"/>
-                                </svg>
+                                <a href="admin-customers-edit.php?id=<?php echo $customer["user_id"]; ?>">
+                                    <svg 
+                                        class="lucide lucide-pencil-line-icon lucide-pencil-line edit"
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        width="24" 
+                                        height="24" 
+                                        viewBox="0 0 24 24" 
+                                        fill="none" 
+                                        stroke="currentColor" 
+                                        stroke-width="2" 
+                                        stroke-linecap="round" 
+                                        stroke-linejoin="round">
+                                        <path d="M12 20h9"/>
+                                        <path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z"/>
+                                        <path d="m15 5 3 3"/>
+                                    </svg>
+                                </a>
                                 <form action="admin-customers.php" method="POST" class="delete-form" style="display:inline-block;">
                                     <input type="hidden" name="user_id" value="<?php echo $customer['user_id']; ?>" />
                                     <button type="submit" name="delete" class="delete-btn">
